99427aa74c9327d2e91d8d2b30df4bf9c53831c1
BrandonLWhite <brandonlwhite@gmail.com>
2025-01-09 11:43:15 -0600

    Add more platform.py tests

--- tests/utils/test_platforms.py
+++ tests/utils/test_platforms.py
@@ -1,3 +1,5 @@
+import pytest
+
 from poetry.utils.env import MockEnv
 import poetry_plugin_bundle.utils.platforms as platforms
 
@@ -43,6 +45,9 @@ def test_create_supported_tags_legacy_manylinux_aliases():
         expected_tags={
             "cp310-cp310-manylinux_2_5_x86_64",
             "cp310-cp310-manylinux_2_1_x86_64",
+            "py312-none-manylinux_2_12_x86_64",
+            "py312-none-any",
+            "cp312-none-any",
         },
         unexpected_tags={
             "cp310-cp310-manylinux_2_6_x86_64",
@@ -72,3 +77,81 @@ def test_create_supported_tags_legacy_manylinux_aliases():
             "cp311-cp311-manylinux_2_24_x86_64",
         }
     )
+
+
+def test_create_supported_tags_macosx():
+    _test_create_supported_tags(
+        platform="macosx_11_0_arm64",
+        python_version_info=(3, 11, 8),
+        expected_tags={
+            "cp311-abi3-macosx_11_0_arm64",
+            "cp311-abi3-macosx_10_12_universal2",
+            "cp311-none-macosx_11_0_universal2",
+            'py311-none-any',
+        },
+        unexpected_tags={
+            "cp311-none-macosx_11_1_universal2",
+        }
+    )
+
+    _test_create_supported_tags(
+        platform="macosx_10_9_x86_64",
+        python_version_info=(3, 11, 8),
+        expected_tags={
+            "cp311-abi3-macosx_10_9_universal2",
+            "cp311-abi3-macosx_10_9_x86_64",
+            "cp311-abi3-macosx_10_9_intel",
+            "cp311-none-macosx_10_7_universal2",
+            'py311-none-any',
+        },
+        unexpected_tags={
+            "cp311-none-macosx_11_1_universal2",
+            "cp311-abi3-macosx_11_0_arm64",
+        }
+    )
+
+
+def test_create_supported_tags_musllinux():
+    _test_create_supported_tags(
+        platform="musllinux_1_1_x86_64",
+        python_version_info=(3, 13, 1),
+        expected_tags={
+            "cp313-cp313-musllinux_1_1_x86_64",
+            "cp313-abi3-musllinux_1_1_x86_64",
+            "py312-none-any",
+            "cp313-none-any",
+        },
+        unexpected_tags={
+            "cp313-cp313-musllinux_1_2_x86_64",
+        }
+    )
+
+
+def test_create_supported_tags_unsupported_platform():
+    env = MockEnv(version_info=(3, 12, 1))
+
+    unsupported_platforms = [
+        "win32",
+        "linux",
+        "foobar",
+    ]
+    for platform in unsupported_platforms:
+        with pytest.raises(NotImplementedError):
+            platforms.create_supported_tags(platform, env)
+
+
+def test_create_supported_tags_malformed_platforms():
+    env = MockEnv(version_info=(3, 12, 1))
+
+    malformed_platforms = [
+        "macosx_11_blah_arm64",
+        "macosx",
+        "manylinux_blammo_12_x86_64",
+        "manylinux_x86_64",
+        "manylinux",
+        "musllinux_?_1_x86_64",
+        "musllinux",
+    ]
+    for platform in malformed_platforms:
+        with pytest.raises(ValueError):
+            platforms.create_supported_tags(platform, env)
